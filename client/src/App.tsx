69bcdfb0f6454d37d71279d054432a60c8851621
denis <>
2025-02-21 12:09:54 +0200

    Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+// Components
+// import { LoginPage } from './Pages/Login';
+import { EntitiesPage } from './Pages/Entities';
+//Styles
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <EntitiesPage />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Entities/index.tsx
@@ -0,0 +1,11 @@
+import { LineChart } from '../../Components/LineChart';
+
+export const EntitiesPage = () => {
+  return (
+    <div>
+      <h1>Entities</h1>
+      
+      <LineChart />
+    </div>
+  );
+};
